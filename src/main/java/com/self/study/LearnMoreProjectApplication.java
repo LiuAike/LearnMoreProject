0657e757d5c28f8bf5640fa784a0e440fbb5e88a
LiuAiKe <lyflycq@163.com>
2020-03-30 15:54:40 +0800

    Initial Commit

---
+++ src/main/java/com/self/study/LearnMoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.self.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMoreProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnMoreProjectApplication.class, args);
+    }
+
+}
